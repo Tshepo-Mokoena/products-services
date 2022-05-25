b6f3c740a0960b32ceb7e3f89f67abfad907d2e1
Tshepo Mokoena <tshepo.william3652@gmail.com>
2022-05-25 11:44:40 +0200

    first commit

---
+++ src/test/java/com/tshepo/ProductsManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tshepo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductsManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
